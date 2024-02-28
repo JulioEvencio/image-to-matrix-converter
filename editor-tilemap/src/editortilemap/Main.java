0d2709d8adafe8e49e581ddeabaceadc877292e6
JulioEvencio <julioigreja.dev@gmail.com>
2024-02-27 23:29:16 -0300

    added: code basic for window

--- editor-tilemap/src/editortilemap/Main.java
+++ editor-tilemap/src/editortilemap/Main.java
@@ -1,9 +1,11 @@
 package editortilemap;
 
+import editortilemap.gui.Editor;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+		new Editor();
 	}
 
 }
---
+++ editor-tilemap/src/editortilemap/util/Util.java
@@ -0,0 +1,13 @@
+package editortilemap.util;
+
+import java.awt.Font;
+
+public class Util {
+
+	public static final String TITLE = "Editor Tilemap";
+
+	public static Font getFontDefault() {
+		return new Font("Sans Serif", Font.BOLD, 25);
+	}
+
+}
